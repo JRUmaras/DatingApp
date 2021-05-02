28686f3e4613a604398ecc7b0bab60fce704b538
Jonas Umaras <jonas.umaras@gmail.com>
2021-05-02 13:01:53 +0300

    Created photo service based on Cloudinary.

--- API/Helpers/CloudinarySettings.cs
+++ API/Helpers/CloudinarySettings.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace API.Helpers
 {
     public class CloudinarySettings
     {
         public string CloudName { get; set; }
+        
         public string ApiKey { get; set; }
+        
         public string ApiSecret { get; set; }
+
+        public Dictionary<string, object> Transformation { get; set; }
     }
 }
