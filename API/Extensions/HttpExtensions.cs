6c9afd1a82da4cc4dee6f39474c5f7b6fc703a8a
Jonas Umaras <jonas.umaras@gmail.com>
2021-05-25 21:18:42 +0300

    Added paging header and user paging settings.

---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using API.Helpers.HttpHeaders;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse httpResponse, int pageNumber, int pageSize, int totalCount, int totalPages)
+        {
+            const string paginationHeaderName = "Pagination";
+
+            var paginationHeader = new PaginationHeader(pageNumber, pageSize, totalCount, totalPages);
+            
+            httpResponse.Headers.Add(paginationHeaderName, JsonSerializer.Serialize(paginationHeader));
+            httpResponse.Headers.Add("Access-Control-Expose-Headers", paginationHeaderName);
+        }
+    }
+}
---
+++ API/Helpers/UserSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace API.Helpers
+{
+    // TODO: move the constants into config. Maybe use user settings factory, which spits out default setup and then lets to adjust the values
+    public class UserSettings
+    {
+        private const int _maxPageSize = 50;
+
+        private int _pageSize = 10;
+
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = Math.Min(_maxPageSize, value);
+        }
+    }
+}
