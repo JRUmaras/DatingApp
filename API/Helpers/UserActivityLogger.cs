81681a23e5f4131f4f6217c7fa79fb27242109be
Jonas Umaras <jonas.umaras@gmail.com>
2021-06-26 12:15:34 +0300

    User activity logger for updating 'last active' datetime.

---
+++ API/Helpers/UserActivityLogger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using API.Extensions;
+using API.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace API.Helpers
+{
+    public class UserActivityLogger : IAsyncActionFilter
+    {
+        private readonly ILogger<UserActivityLogger> _logger;
+
+        public UserActivityLogger(ILogger<UserActivityLogger> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+
+            var identity = resultContext.HttpContext.User.Identity;
+            if (identity is null || !identity.IsAuthenticated) return;
+
+            var username = resultContext.HttpContext.User.GetUsername();
+            var userRepo = resultContext.HttpContext.RequestServices.GetService<IUserRepository>();
+            if (userRepo is null)
+            {
+                _logger.Log(LogLevel.Warning, "Failed to inject user repository.");
+                return;
+            }
+
+            var user = await userRepo.GetUserByUsernameAsync(username);
+            user.LastActive = DateTime.UtcNow;
+            await userRepo.SaveAllAsync();
+        }
+    }
+}
