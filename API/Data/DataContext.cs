9e00c1a1baa697cecb219b3396e8f5c9a1601445
Jonas Umaras <jonas.umaras@gmail.com>
2021-07-18 16:57:19 +0300

    Added Likes table and configured the entity.

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -9,5 +9,34 @@ namespace API.Data
         { }
 
         public DbSet<AppUser> Users { get; set; }
+
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            ConfigureLikesEntity(modelBuilder);
+        }
+
+        private static void ConfigureLikesEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserLike>()
+                .HasKey(userLike => new { SourceUserId = userLike.LikerId, DestinationUserId = userLike.LikeeId });
+
+            // Liked-users relationship
+            modelBuilder.Entity<UserLike>()
+                .HasOne(userLike => userLike.Liker)
+                .WithMany(user => user.LikedUsers)
+                .HasForeignKey(userLike => userLike.LikerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Liked-by-users relationship
+            modelBuilder.Entity<UserLike>()
+                .HasOne(userLike => userLike.Likee)
+                .WithMany(user => user.LikedByUsers)
+                .HasForeignKey(userLike => userLike.LikeeId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
---
+++ API/Entities/UserLike.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Entities
+{
+    public class UserLike
+    {
+        public AppUser Liker { get; set; }
+
+        public int LikerId { get; set; }
+
+        public AppUser Likee { get; set; }
+
+        public int LikeeId { get; set; }
+    }
+}
